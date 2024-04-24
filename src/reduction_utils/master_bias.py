44affff68f6ea6ab7f18bae5057637f6b0c76407
StaudtPatrick <staudtpat@yahoo.de>
2024-04-24 09:31:14 +0200

    added the possiblilty to save figures, changed the iteration on plots to begin with 1 and changed the plot names

--- src/reduction_utils/master_bias.py
+++ src/reduction_utils/master_bias.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import numpy as np
 import argparse
+import sys
+import os 
+import glob
+from PIL import Image
+from pathlib import Path
 
 # Prevent matplotlib plotting frames upside down
 plt.rcParams['image.origin'] = 'lower'
@@ -15,7 +20,15 @@ parser.add_argument('-v','--verbose',help="""Display the image of each bias fram
 parser.add_argument('-inst','--instrument',help="""MUST define which instrument we're using, either ACAM or EFOSC""")
 parser.add_argument('-c','--clobber',help="""Need this argument to save resulting fits file, default = False""",action='store_true')
 parser.add_argument('-e','--eyeball',help="""Use this argument to specify whether bias frames are to be eyeballed - sorting into good and bad frames.""",action='store_true')
+parser.add_argument('-s','--savefig',help="""Use this argument to save the bias fits to an gif, default=False""",action='store_true')
 args = parser.parse_args()
+
+if args.savefig==True and args.verbose==False:
+    print('Attention: You must put verbose=True to store the files!')
+    sys.exit(1)
+
+if args.clobber==False and os.path.exists("bias/master_bias.fits"):
+    print("Attention: The master_bias.fits file will not be updated. Redo the task with clobber!")
 
 if args.instrument != 'EFOSC' and args.instrument != 'ACAM':
     raise NameError('Currently only set up to deal with ACAM or EFOSC data')
@@ -30,16 +43,21 @@ if args.instrument == 'ACAM':
     test = fits.open(test_file)
     nwin = len(test) - 1
 
+# Creates new subdirectory for saving outputs
+Path('./bias').mkdir(parents=True, exist_ok=True)
 
+bias_files = np.loadtxt(args.biaslist,str)
 
-def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False):
+frame_counter = 1
+
+def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False,savefig=False):
     """median combine biases which were taken with a single window"""
-    bias_files = np.loadtxt(args.biaslist,str)
+    global frame_counter
 
     bias_data = []
 
     if verbose or eyeball:
-        plt.figure()
+        plt.figure(figsize=(11,7))
 
     if eyeball:
         good_frames = []
@@ -54,18 +72,21 @@ def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False):
         if instrument == 'EFOSC':
             data_frame = i[0].data
 
-        print('File #%d/%d ; %s ; Mean = %.1f ; Variance = %.1f ; var/mean = %.2f'%(n,len(bias_files)-1,f[-12:],np.mean(data_frame),np.var(data_frame),np.var(data_frame)/np.mean(data_frame)))
+        print('File #%d/%d ; %s ; Mean = %.1f ; Variance = %.1f ; var/mean = %.2f'%(n+1,len(bias_files),f[-18:-5],np.mean(data_frame),np.var(data_frame),np.var(data_frame)/np.mean(data_frame)))
 
         bias_data.append(data_frame)
 
         if verbose or eyeball:
-
-            plt.title('#%d/%d ; %s'%(n,len(bias_files)-1,f[-12:]))
+            plt.title('#%d/%d ; %s'%(n+1,len(bias_files),f[-18:-5]))
             plt.imshow(data_frame,vmin=np.median(data_frame)*0.99,vmax=np.median(data_frame)*1.01)
             plt.xlabel("X pixel")
             plt.ylabel("Y pixel")
             plt.colorbar()
-            plt.show(block=False)
+            if not eyeball and savefig:
+                plt.savefig('bias/' + '%s'%(f[-34:-5]) + '.png')
+                frame_counter += 1
+
+            plt.show(block=False) 
 
             if eyeball: # plot each frame and have user select what is good and bad. NEEDS IMPLEMENTING FOR 2 WINDOWS.
                 plt.pause(0.5)
@@ -75,6 +96,8 @@ def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False):
                     frame_quality = input("good/bad? [g/b]: ")
                     if frame_quality == 'g':
                         good_frames.append(f)
+                        if savefig:
+                            plt.savefig('bias/' + '%s'%(f[-34:-5]) + '.png')
                         break
                     if frame_quality == 'b':
                         bad_frames.append(f)
@@ -84,8 +107,8 @@ def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False):
 
             else:
                 plt.pause(1.0)
-                plt.clf()
-
+                plt.clf()                
+            
         i.close()
 
     if eyeball:
@@ -115,8 +138,6 @@ def combine_biases_1window(bias_list,instrument,verbose=False,eyeball=False):
 def combine_biases_2windows(bias_list,verbose=False):
     """median combine biases which were taken with two windows"""
 
-    bias_files = np.loadtxt(args.biaslist,str)
-
     bias_data = [[],[]]
 
     if verbose:
@@ -139,10 +160,10 @@ def combine_biases_2windows(bias_list,verbose=False):
                 plt.imshow(data_frame,vmin=np.median(data_frame)*0.99,vmax=np.median(data_frame)*1.01)
                 plt.colorbar()
                 if level == 1:
-	                plt.xlabel("X pixel")
-	                plt.ylabel("Y pixel")
-	            else:
-					plt.yticks(visible=False)
+                    plt.xlabel("X pixel")
+                    plt.ylabel("Y pixel")
+                else:
+                    plt.yticks(visible=False)
 
             print('File = %s ; Mean (window %d) = %f ; Shape (window %d) = %s'%(f,level,np.mean(data_frame),level,(np.shape(data_frame))))
 
@@ -160,20 +181,42 @@ def combine_biases_2windows(bias_list,verbose=False):
 
     return median_combine
 
-def save_fits(data,filename,clobber=False):
+def save_fits(data,filename):
     hdu = fits.PrimaryHDU(data)
-    hdu.writeto(filename,overwrite=clobber)
+    hdu.writeto(filename,overwrite=True)
     return
 
 if nwin == 1:
-    master_bias = combine_biases_1window(args.biaslist,args.instrument,args.verbose,args.eyeball)
+    master_bias = combine_biases_1window(args.biaslist,args.instrument,args.verbose,args.eyeball,args.savefig)
+    if args.savefig:
+        # Create a GIF using Pillow
+        image_files = sorted(glob.glob('bias/EFOSC*.png'))
+        images = [Image.open(image_file) for image_file in image_files]
+
+        # Specify the output file path
+        output_gif_path = 'bias/bias_movie.gif'
+
+        # Save as a GIF
+        images[0].save(output_gif_path, save_all=True, append_images=images[1:], duration=500, loop=0)
+
+        #delete the simple files
+        all_files = os.listdir("bias")
+        for file in all_files:
+            if file.startswith("EFOSC"):
+                os.remove(os.path.join("bias",file))
+
 else:
     master_bias = combine_biases_2windows(args.biaslist,args.verbose)
 
-save_fits(master_bias,'master_bias.fits',args.clobber)
+if args.verbose:
+    plt.close()
+
+if args.clobber:
+    save_fits(master_bias,'bias/master_bias.fits')
 
 ## Plot master bias
-plt.figure()
+plt.figure(figsize=(11,7))
+plt.title('Master Bias', size=16)
 if nwin == 1:
      plt.imshow(master_bias,vmin=np.median(master_bias)*0.99,vmax=np.median(master_bias)*1.01)
      plt.ylabel("Y pixel")
@@ -188,4 +231,6 @@ else:
      plt.yticks(visible=False)
 
 plt.colorbar()
+if args.clobber:
+    plt.savefig('bias/master_bias.png')
 plt.show()
